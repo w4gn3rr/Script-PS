172e5e6014850edba38a2f1ed2df48823f548372
w4gn3rr <w4gn3rr@gmail.com>
2019-07-26 11:15:07 -0300

    Modificado

--- Status_usuario.ps1
+++ Status_usuario.ps1
@@ -1,5 +1,5 @@
 ﻿$Nome = Read-Host "Digite o somente o nome de rede do usuario:"
-$Dominio = "@cellereit.com.br"
+$Dominio = "@dominio.com.br"
 $Usuario = $Nome + $Dominio
 Write-Host  "Verificando dados do(a) usuario(a) $Usuario"
 Get-MSOlUser  -UserPrincipalName "$Usuario"  | Select-Object CloudExchangeRecipientDisplayType , UserPrincipalName, DisplayName, LastName, LastPasswordChangeTimestamp, @{Name=”DuracaodaSenha”;Expression={(Get-Date)-$_.LastPasswordChangeTimeStamp}}, ValidationStatus   , WhenCreated    , BlockCredential, PasswordNeverExpires, PasswordResetNotRequiredDuringActivate , LicenseReconciliationNeeded  , StrongPasswordRequired       , StsRefreshTokensValidFrom    ,  Licenses , ObjectId , Office , OverallProvisioningStatus , PreferredLanguage  , ProxyAddresses , LicenseAssignmentDetails , SignInName     , Title, UsageLocation  , UserLandingPageIdentifierForO365Shell  , UserThemeIdentifierForO365Shell , UserType , City 
