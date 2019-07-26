ff4cb49dfc9a398b7509358724a5420bd57129e3
w4gn3rr <w4gn3rr@gmail.com>
2019-07-26 11:16:29 -0300

    modificado2

--- Status_usuario.ps1
+++ Status_usuario.ps1
@@ -3,3 +3,4 @@ $Dominio = "@dominio.com.br"
 $Usuario = $Nome + $Dominio
 Write-Host  "Verificando dados do(a) usuario(a) $Usuario"
 Get-MSOlUser  -UserPrincipalName "$Usuario"  | Select-Object CloudExchangeRecipientDisplayType , UserPrincipalName, DisplayName, LastName, LastPasswordChangeTimestamp, @{Name=”DuracaodaSenha”;Expression={(Get-Date)-$_.LastPasswordChangeTimeStamp}}, ValidationStatus   , WhenCreated    , BlockCredential, PasswordNeverExpires, PasswordResetNotRequiredDuringActivate , LicenseReconciliationNeeded  , StrongPasswordRequired       , StsRefreshTokensValidFrom    ,  Licenses , ObjectId , Office , OverallProvisioningStatus , PreferredLanguage  , ProxyAddresses , LicenseAssignmentDetails , SignInName     , Title, UsageLocation  , UserLandingPageIdentifierForO365Shell  , UserThemeIdentifierForO365Shell , UserType , City 
+teste 2
